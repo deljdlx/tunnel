3bd2bfaf12449b1f0adef749e4f35b44860285bb
Julien - Biniou <julien.delsescaux@oclock.io>
2020-02-10 10:22:45 +0100

    book exemple

---
+++ data/book.php
@@ -0,0 +1,17 @@
+<?php
+$bibliothèque = [
+
+    'berlin' => [
+        'titre' => 'Berlin en quelques jours',
+        'auteur' => 'Lonely Planet',
+        'categorie' => 'tourisme',
+        'synopsis' => 'quoi faire à Berlin?',
+    ],
+
+    'monkFerrari' => [
+        'titre' => 'The Monk who sold his Ferrari',
+        'auteur' => 'Robin Sharma',
+        'categorie' => 'Developpement personnel',
+        'synopsis' => 'comment être heureux',
+    ],
+];
